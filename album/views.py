8e3f33497c3d1fd993bfa0e9208552a07331e921
Juan <juanjo.figue97@gmail.com>
2024-07-06 12:42:54 -0500

    Creacion proyecto Copa America

---
+++ album/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy 
+from django.views.generic import CreateView, DeleteView, UpdateView, ListView, DetailView
+from album.models import Team, Player
+
+# Create your views here.
+class TeamListView(ListView):
+    model = Team
+
+class TeamDetailView(DetailView):
+    model = Team
+
+class PlayerListView(ListView):
+    model = Player
+
+class PlayerDetailView(DetailView):
+    model = Player
